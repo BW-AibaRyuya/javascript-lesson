775adbab51a403fe1458a681d82f2ffc5922b375
BW-AibaRyuya <14125.ryuya1123@gmail.com>
2025-08-25 21:03:02 +0900

    Q5修正

--- question/app.js
+++ question/app.js
@@ -132,18 +132,16 @@ for (let i = 0; i < 100; i++) {
 }
 
 console.log(numbers);
-// Q5
+// Q5 修正
 let mixed = [4, '2', 5, '8', '9', 0, 1];
 
 for (let i = 0; i < mixed.length; i++) {
   let item = mixed[i];
 
-  if (typeof item === 'number') {
-    if (item % 2 === 0) {
-      console.log('even');
-    } else {
-      console.log('odd');
-    }
+  if (typeof item === 'number' && item % 2 === 0) {
+    console.log('even');
+  } else if (typeof item === 'number' && item % 2 !== 0) {
+    console.log('odd');
   } else {
     console.log('not number');
   }
